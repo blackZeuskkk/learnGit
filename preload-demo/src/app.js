469408a4c6e8cb7c28ab26da75a16674c52964e7
zhaokai.kk <zhaokai.kk@bytedance.com>
2024-04-02 19:20:58 +0800

    feat: 学习webpack压缩图片

---
+++ preload-demo/src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Icon from "../access/images/1.png";
+import Icon1 from "../access/images/1.svg";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <img src={Icon}></img>
+        <img src={Icon1}></img>
+      </div>
+    );
+  }
+}
+
+export default App;
